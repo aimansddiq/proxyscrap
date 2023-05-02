f6499ba4aa93ac39656f70bd1ca76140e9215a72
Qidds <aimansddiq@gmail.com>
2023-05-02 17:10:26 +0800

    Add proxies folder

--- proxyscrape_scrape.py
+++ proxyscrape_scrape.py
@@ -1,5 +1,11 @@
 import urllib.request
 import requests
+import os
+
+folder_name = "proxies"
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+    print("Folder created: ", folder_name)
 
 timeout = 10000 #put your own timeout here
 
@@ -8,4 +14,4 @@ for x in proxytype:
     proxies = requests.get('https://api.proxyscrape.com?request=amountproxies&proxytype={}'.format(x))
     url = 'https://api.proxyscrape.com?request=getproxies&proxytype={}&timeout={}'.format(x,timeout)
     print('Scraping '+ proxies.text +'\t' + x + ' proxies...')
-    urllib.request.urlretrieve(url, '{}.txt'.format(x))
+    urllib.request.urlretrieve(url, 'proxies/{}.txt'.format(x))
